86d5ca76218fd1158bb5bafc885b0693032bfcea
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-01-28 17:59:55 +0200

    added buttons, defined defaults and variantes, added twMerge

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,14 @@
+import { Menu } from "lucide-react";
 import logo from "../assets/logo.png";
+import { Button } from "../components/Button";
 
 export function PageHeader() {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div className="flex gap-4 items-centre flex-shrink-0">
-        <button></button>
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
         <a href="/">
           <img src={logo} className="h-6" />
         </a>
