3f124dfd86a79e57f16013c86056bc105fe4b90f
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-02-01 19:33:07 +0200

    bug fixing and render issues resolved in SidebarSmall, imported playlist data from Sidebar.ts. More bug fixes needed in Sidebar

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,9 +1,21 @@
-import { Clapperboard, Home, Library, Repeat } from "lucide-react";
-import { ElementType } from "react";
-import { buttonStyles } from "../components/Button";
+import {
+  ChevronDown,
+  ChevronUp,
+  Clapperboard,
+  Clock,
+  History,
+  Home,
+  Library,
+  ListVideo,
+  PlaySquare,
+  Repeat,
+} from "lucide-react";
+import { ElementType, useState } from "react";
+import { Button, buttonStyles } from "../components/Button";
 import { twMerge } from "tailwind-merge";
 import { ReactNode } from "react";
 import { Children } from "react";
+import { playlists } from "../data/sidebar";
 
 export function Sidebar() {
   return (
@@ -19,8 +31,36 @@ export function Sidebar() {
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
       <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex ">
-        <LargeSidebarSection>
+        <LargeSidebarSection visibleItemCount={3}>
           <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem
+            Icon={Clapperboard}
+            title="Subscriptions"
+            url="/subscriptions"
+          />
+        </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection visibleItemCount={5}>
+          <LargeSidebarItem Icon={Library} title="Library" url="/library" />
+          <LargeSidebarItem Icon={History} title="History" url="/history" />
+          <LargeSidebarItem
+            Icon={PlaySquare}
+            title="Your Videos"
+            url="/your-videos"
+          />
+          <LargeSidebarItem
+            Icon={Clock}
+            title="Watch Later"
+            url="/playlist?list=WL"
+          />
+          {playlists.map((playlist) => (
+            <LargeSidebarItem
+              key={playlist.id}
+              Icon={ListVideo}
+              title={playlist.name}
+              url={`/playlist?list=${playlist.id}`}
+            />
+          ))}
         </LargeSidebarSection>
       </aside>
     </>
@@ -59,10 +99,30 @@ function LargeSidebarSection({
   title,
   visibleItemCount = Number.POSITIVE_INFINITY,
 }: LargeSidebarSectionProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const childrenArray = Children.toArray(children).flat();
-  const visibileChildren = childrenArray;
+  const showExpandButton = childrenArray.length > visibleItemCount;
+  const visibleChildren = isExpanded
+    ? childrenArray
+    : childrenArray.slice(0, visibleItemCount);
+  const ButtonIcon = isExpanded ? ChevronUp : ChevronDown;
 
-  return <div></div>;
+  return (
+    <div>
+      {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+      {visibleChildren}
+      {showExpandButton && (
+        <Button
+          onClick={() => setIsExpanded((e) => !e)}
+          variant="ghost"
+          className="w-full flex items-center rounded-lg gap-4 p-3"
+        >
+          <ButtonIcon className="w-6 h-6" />
+          <div>{isExpanded ? "Show Less" : "Show More"}</div>
+        </Button>
+      )}
+    </div>
+  );
 }
 
 type LargesidebarItemProps = {
